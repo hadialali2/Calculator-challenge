70bca4a2abcfdfc3cacb9fafe6da9cd3d7eecd5a
hadialali2 <144792995+hadialali2@users.noreply.github.com>
2023-09-18 01:32:42 +0300

    HADI ALALI Calculator-challenge
    HADI ALALI Calculator-challenge

---
+++ calculator/src/components/Model.jsx
@@ -0,0 +1,21 @@
+// src/components/Modal.js
+
+import React from "react";
+import "./Modal.css";
+
+function Modal({ isOpen, onClose, children }) {
+  if (!isOpen) return null;
+
+  return (
+    <div className="modal-overlay">
+      <div className="modal">
+        <button className="close-button" onClick={onClose}>
+          X
+        </button>
+        {children}
+      </div>
+    </div>
+  );
+}
+
+export default Modal;
